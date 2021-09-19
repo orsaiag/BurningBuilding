824eaaef5ede2ee7f89e00de32584d819c8d33c5
AmitNShani <shniedy@gmail.com>
2021-09-19 14:35:49 +0300

    #8

--- app/src/main/java/com/example/burningbuilding/Floor1Activity.java
+++ app/src/main/java/com/example/burningbuilding/Floor1Activity.java
@@ -6,6 +6,7 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -20,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -30,32 +32,90 @@ import java.util.ListIterator;
 import java.util.Queue;
 
 public class Floor1Activity extends AppCompatActivity {
-    List<Animator> animatorList = new ArrayList<>();
-    Boolean isRunning = false;
 
     CountDownTimer gameTimer;
     long milisecondsOfGame;
+    TextView letterNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor1);
 
+        letterNumber = findViewById(R.id.number_of_letter);
         ImageButton playButton = findViewById(R.id.play_button_morse);
         ImageButton openDoorButton = findViewById(R.id.open_door_IB);
         EditText passcoceTextField = findViewById(R.id.pass_code_tf);
+
         ImageView dotIV=findViewById(R.id.dot_button);
         ImageView lineIV=findViewById(R.id.line_button);
 
-        ObjectAnimator letterO = ObjectAnimator.ofFloat(lineIV, "alpha", 0.1f, 1f).setDuration(1000);
+        ObjectAnimator letterO = ObjectAnimator.ofFloat(lineIV, "alpha", 1f, 0.1f).setDuration(1000);
+        ObjectAnimator dotReturnAnim = ObjectAnimator.ofFloat(dotIV, "alpha", 1f);
+        ObjectAnimator lineReturnAnim = ObjectAnimator.ofFloat(lineIV, "alpha", 1f);
+
         letterO.setRepeatCount(2);
-        ObjectAnimator letterS = ObjectAnimator.ofFloat(dotIV, "alpha", 0.1f, 1f).setDuration(1000);
+        ObjectAnimator letterS = ObjectAnimator.ofFloat(dotIV, "alpha", 1f, 0.1f).setDuration(1000);
         letterS.setRepeatCount(2);
+        letterO.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                    letterNumber.setText(Integer.parseInt(letterNumber.getText().toString()) + 1 + "");
+                lineReturnAnim.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        letterS.addListener(new Animator.AnimatorListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (letterNumber.getText().toString().equals("3")){
+                    letterNumber.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    letterNumber.setText(Integer.parseInt(letterNumber.getText().toString()) + 1 + "");
+                }
+                dotReturnAnim.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
 
         AnimatorSet code = new AnimatorSet();
-        code.play(letterS).before(letterO).after(3000);
+        code.play(letterS).after(1000);
         AnimatorSet code2 = new AnimatorSet();
-        code2.play(letterS).after(9000);
+        code2.play(letterO).after(5000);
+        AnimatorSet code3 = new AnimatorSet();
+        code3.play(letterS).after(9000);
 
         Intent intent = getIntent();
         milisecondsOfGame = intent.getLongExtra("timer",600000);
@@ -82,8 +142,22 @@ public class Floor1Activity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                letterNumber.setText("0");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        letterNumber.setText("1");
+                        letterNumber.setVisibility(View.VISIBLE);
+
+
+                    }
+                }, 1000);
+
+
                 code.start();
                 code2.start();
+                code3.start();
+
             }
         });
         openDoorButton.setOnClickListener(new View.OnClickListener() {
